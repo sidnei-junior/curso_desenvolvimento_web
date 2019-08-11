c37d80a9d543a59ac7b4408d54c18efdc33770c8
sidneiJr2017 <sidnei.ifrn@gmail.com>
2019-08-11 03:10:58 -0300

    Décima Atualização. Aprendi o básico de PHP

---
+++ curso - 01/1. Praticando o conteúdo/10_php/mensagens_divertidas.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+	<head>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" type="text/css" href="estilo.css">
+		<title>Mensagens divertidas</title>
+	</head>
+
+	<body>
+		<?php 
+			echo 'Teste de impressão com echo <br>';
+			print 'Teste de impressão com print';
+		?>
+	</body>
+</html>
