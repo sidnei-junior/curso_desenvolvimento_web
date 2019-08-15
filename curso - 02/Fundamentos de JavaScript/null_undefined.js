43d3b848a6097d3b60729d1354ae0e41f1f42df7
sidneiJr2017 <sidnei.ifrn@gmail.com>
2019-08-15 14:03:20 -0300

    Aprendi sobre o null e o undefined e sobre usar funções

---
+++ curso - 02/Fundamentos de JavaScript/null_undefined.js
@@ -0,0 +1,14 @@
+let valor // não inicializado
+console.log(valor)
+
+valor = null // ausência de valor
+console.log(valor)
+//console.log(valor.toString())
+
+const produto = {}
+
+console.log(produto.preco)
+console.log(produto)
+
+produto.preco = 3.50
+console.log(produto.preco)
