52fdf9827a2d83046597a9334d5d531221bd20c1
sidneiJr2017 <sidnei.ifrn@gmail.com>
2019-09-08 07:29:55 -0300

    Finalizado capítulo de Array

---
+++ curso - 02/5. array/concat.js
@@ -0,0 +1,8 @@
+const filhas = ['Ualeskah', 'Cibalena']
+const filhos = ['Uoxiton', 'Uesclei']
+
+const todos = filhas.concat(filhos, 'Fulano')
+
+console.log(todos)
+
+console.log ([].concat([1,2], [3,4], 5, [[6, 7]]))
