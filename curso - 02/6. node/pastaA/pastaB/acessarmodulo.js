89ba2d3dc76584d97dc62d84394f7c7d4980f176
sidneiJr2017 <sidnei.ifrn@gmail.com>
2019-09-09 06:10:45 -0300

    Usando Modulo de Terceiros. Aprendendo a manipular os caminhos para pastas

---
+++ curso - 02/6. node/pastaA/pastaB/acessarmodulo.js
@@ -0,0 +1,13 @@
+const moduloA = require('../../moduloA')
+console.log(moduloA.ateLogo)
+
+const c = require('./pastaC')
+console.log(c.ola2)
+
+/*
+const http = require ('http')
+http.createServer((req, res) => {
+    res.write('Bom dia!')
+    res.end()
+}).listen(8080)
+*/
