8c5d17746bdb3d11c9156edb241c8f17a726cb55
sidneiJr2017 <sidnei.ifrn@gmail.com>
2019-09-14 20:47:35 -0300

    começo de uso de Express

---
+++ curso - 02/6. node/projeto/src/servidor.js
@@ -0,0 +1,18 @@
+const porta = 3003
+
+const express = require('express')
+const app = express()
+
+app.get('/produtos', (req, res, next) => {
+    console.log("Middlware 1...")
+    next()
+})
+
+
+app.get('/produtos', (req, res, next) => {
+    res.send({ nome: 'Notebook', preco: 123.45}) // Converter para JSON
+})
+
+app.listen(porta, () => {
+    console.log(`Servidor está executando na porta ${porta}.`)
+})
