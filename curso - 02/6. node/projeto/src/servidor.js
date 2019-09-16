d140c1169cc29e7eed5f370da9d838c338944f99
sidneiJr2017 <sidnei.ifrn@gmail.com>
2019-09-16 11:41:32 -0300

    Feito primeiro CRUD com node e testando no POSTMAN

--- curso - 02/6. node/projeto/src/servidor.js
+++ curso - 02/6. node/projeto/src/servidor.js
@@ -2,15 +2,39 @@ const porta = 3003
 
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+const bancoDeDados = require('./bancoDeDados')
+
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/produtos', (req, res, next) => {
-    console.log("Middlware 1...")
-    next()
+    res.send(bancoDeDados.getProdutos()) // Converter para JSON
 })
 
+app.get('/produtos/:id', (req, res, next) => {
+    res.send(bancoDeDados.getProduto(req.params.id))
+})
 
-app.get('/produtos', (req, res, next) => {
-    res.send({ nome: 'Notebook', preco: 123.45}) // Converter para JSON
+app.post('/produtos', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+    res.send(produto) // JSON
+})
+
+app.put('/produtos/:id', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        id: req.params.id,
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+    res.send(produto) // JSON
+})
+
+app.delete('/produtos/:id', (req, res, next) => {
+    const produto = bancoDeDados.excluirProduto(req.params.id)
+    res.send(produto) // JSON
 })
 
 app.listen(porta, () => {
