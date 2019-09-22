890ef993d4a7cc9608b9cd4f8027680977dfa3e5
sidneiJr2017 <sidnei.ifrn@gmail.com>
2019-09-21 23:11:06 -0300

    Adicionado arquivos de novas funções que vinheram na atualizações do ESNext

---
+++ curso - 02/7. esnext/set.js
@@ -0,0 +1,12 @@
+const times = new Set()
+times.add('Vasco')
+times.add('São Paulo').add('Palmeiras').add('Corinthians')
+times.add('Flamengo')
+times.add('Vasco')
+
+console.log(times)
+console.log(times.size)
+console.log(times.has('vasco'))
+console.log(times.has('Vasco'))
+times.delete('Flamengo')
+console.log(times.has('Flamengo'))
