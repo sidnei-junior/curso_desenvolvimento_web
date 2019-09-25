4a61b5ee926b04a12c813f8a81357461330bb3b0
sidneiJr2017 <sidnei.ifrn@gmail.com>
2019-09-25 05:18:39 -0300

    Adicionado mudanças para mostrar a model itens

--- curso - 03/django/produtos/itens/views.py
+++ curso - 03/django/produtos/itens/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def home(request):
+    return HttpResponse('<h1>Área de home</h1>')
+
+def produtos(request):
+    return HttpResponse('<h1>Área de Produto</h1>')
+
+def clientes(request):
+    return HttpResponse('<h1>Área de Cliente</h1>')
--- curso - 03/django/produtos/produtos/urls.py
+++ curso - 03/django/produtos/produtos/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from itens.views import home, produtos, clientes
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/', home),
+    path('produtos/', produtos),
+    path('clientes/', clientes),
 ]
