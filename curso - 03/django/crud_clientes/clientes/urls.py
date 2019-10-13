57db3222096fd585070c9a42a340f6c217ffcaa9
sidneiJr2017 <sidnei.ifrn@gmail.com>
2019-10-13 08:13:54 -0300

    :sparkles: Concluido o curso com um CRUD no squlite com django

--- curso - 03/django/crud_clientes/clientes/urls.py
+++ curso - 03/django/crud_clientes/clientes/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import lista_de_clientes, adicionar_cliente
+from .views import lista_de_clientes, adicionar_cliente, editar_cliente, remover_cliente
 urlpatterns = [
-    path('', lista_de_clientes),
-    path('adicionar_cliente/',adicionar_cliente)
+    path('', lista_de_clientes, name='lista_de_clientes'),
+    path('adicionar_cliente/',adicionar_cliente, name='adicionar_cliente'),
+    path('editar_cliente/<int:id>', editar_cliente),
+    path('remover_cliente/<int:id>', remover_cliente)
 ]
--- curso - 03/django/crud_clientes/clientes/views.py
+++ curso - 03/django/crud_clientes/clientes/views.py
@@ -1,10 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect, get_object_or_404
 from .models import Cliente
 from .forms import ClienteForm
+from django.contrib import messages
+from django.db.models import Q
+from django.core.paginator import Paginator
+
 # Create your views here.
 
 def lista_de_clientes(request):
-   clientes = Cliente.objects.all()
+   clientes = Cliente.objects.all().order_by('-id')
+   queryset = request.GET.get('q')
+   if queryset:
+      clientes = Cliente.objects.filter(
+         Q(nome__icontains = queryset)|
+         Q(email__icontains = queryset)|
+         Q(cpf__icontains = queryset)
+      ).order_by('-id')
+
+   paginator = Paginator(clientes, 5) # Show 5 contacts per page
+   page = request.GET.get('page')
+   clientes = paginator.get_page(page)
+
    return render(request, "clientes/lista_de_clientes.html",{'clientes': clientes})
 
 def adicionar_cliente(request):
@@ -12,7 +28,28 @@ def adicionar_cliente(request):
    if form.is_valid():
       obj = form.save()
       obj.save()
-   
-   form = ClienteForm()
+      form = ClienteForm()
+      messages.success(request, "Cliente adicionado com sucesso")
+      return redirect('lista_de_clientes')
 
    return render(request, "clientes/adicionar_clientes.html", {'form':form})
+
+def editar_cliente(request, id=None ):
+   cliente = get_object_or_404(Cliente, id = id)
+   form = ClienteForm(request.POST or None,instance=cliente)
+   if form.is_valid():
+      obj = form.save()
+      obj.save()
+      messages.info(request, "Cliente editado com sucesso")
+      return redirect('lista_de_clientes')
+
+   return render(request, "clientes/editar_cliente.html", {'form':form})
+
+def remover_cliente(request, id=None):
+   cliente = get_object_or_404(Cliente, id=id)
+   if request.method == "POST":
+      cliente.delete()
+      messages.warning(request, "Cliente excluido com sucesso")
+      return redirect('lista_de_clientes')
+
+   return render(request, "clientes/remover_cliente.html", {'cliente':cliente})
